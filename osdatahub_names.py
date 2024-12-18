d6877828f71254c839fa1889ab88b2c00530d538
Richard Hull <rm_hull@yahoo.co.uk>
2024-12-18 00:13:58 +0000

    Log DataHub failures in github action

--- osdatahub_names.py
+++ osdatahub_names.py
@@ -22,14 +22,19 @@ class OSDataHub:
                 "point": f"{easting:0.0f},{northing:0.0f}",
                 # "fq": f"LOCAL_TYPE:Postcode"
             },
-        ).json()
+        )
 
-        if resp["header"]["totalresults"] != 1:
+        json_payload = resp.json()
+        if "header" not in json_payload:
+            print(f"PANIC! unexpected payload\n{resp.text}")
+            exit(-1)
+
+        if json_payload["header"]["totalresults"] != 1:
             return None
 
-        # return (resp["results"][0]["GAZETTEER_ENTRY"],)
+        # return (json_payload["results"][0]["GAZETTEER_ENTRY"],)
         return pick_attributes(
-            resp["results"][0]["GAZETTEER_ENTRY"],
+            json_payload["results"][0]["GAZETTEER_ENTRY"],
             keys=[
                 "NAME1",
                 "LOCAL_TYPE",
@@ -61,12 +66,7 @@ if __name__ == "__main__":
     print(json.dumps(datahub.nearby(56.3779128789, -3.9830157992), indent=2))
     print(json.dumps(datahub.nearby(53.2266182629, -4.0030570596), indent=2))
     print(json.dumps(datahub.nearby(53.9934096, -1.5331361), indent=2))
-    print(json.dumps(datahub.nearby(53.9653160,-1.5574571), indent=2))
+    print(json.dumps(datahub.nearby(53.9653160, -1.5574571), indent=2))
     print(json.dumps(datahub.nearby(53.9771144, -1.6432486), indent=2))
     print(json.dumps(datahub.nearby(53.9358396, -2.5691932), indent=2))
     print(json.dumps(datahub.nearby(53.4656173, -2.3283789), indent=2))
-
-
-
-
-
