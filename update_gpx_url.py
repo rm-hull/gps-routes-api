ffbcec9395b1b57d3d7a3ce8f3b1f02dc69d6331
Richard Hull <rm_hull@yahoo.co.uk>
2024-12-24 23:04:04 +0000

    Update some more new routes

---
+++ update_gpx_url.py
@@ -0,0 +1,18 @@
+import json
+from tqdm import tqdm
+
+from utils import walk_files
+
+for file in tqdm(list(walk_files("data/backup")), desc="Updating GPX URL", unit="record"):
+    with open(file, "r") as fp:
+        record = json.load(fp)
+
+    object_id = record["objectID"]
+
+    if "gpx_url" in record:
+        record["gpx_url"] = (
+            f"https://raw.githubusercontent.com/rm-hull/gps-routes-api/refs/heads/main/data/routes/{object_id[0]}/{object_id}.gpx"
+        )
+
+    with open(file, "w") as fp:
+        json.dump(record, fp, indent=2)
