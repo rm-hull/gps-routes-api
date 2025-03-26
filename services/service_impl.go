fbd18060a9f056237d6994b2e4949bb0a5fe2b48
Richard Hull <rm_hull@yahoo.co.uk>
2025-03-26 01:18:39 +0000

    Update attribution

--- services/service_impl.go
+++ services/service_impl.go
@@ -109,6 +109,8 @@ func (service *RoutesServiceImpl) Search(criteria *model.SearchRequest) (*model.
 			"GPS Cycle and Walking Routes: https://gps-routes.co.uk",
 			"OS DataHub: Names API. https://www.ordnancesurvey.co.uk",
 			"Nominatim: Data © OpenStreetMap contributors, ODbL 1.0. http://osm.org/copyright",
+			"Llama.cpp: https://github.com/ggml-org/llama.cpp/blob/master/LICENSE",
+			"lmstudio-community/gemma-3-4b-it-Q8_0.gguf: https://ai.google.dev/gemma/terms",
 		},
 	}, nil
 }
